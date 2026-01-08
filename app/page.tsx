14d272e6787021ff56e551bc4893e845c91b7327
Mofe-Bankole <bankoleadedotun16@gmail.com>
2026-01-08 22:54:12 +0100

    Added Blog

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,69 @@
-'use client'
-import Header from "@/components/Header";
+"use client";
 import WalletConnect from "@/components/WalletConnect";
+import { useWallet } from "@lazorkit/wallet";
+import Link from "next/link";
 
 export default function Home() {
+  // Apps main page
+
+  const Xlink = "https://x.com/Mofe_bnks";
+
+  const { wallet } = useWallet();
+
   return (
-    <div className=" bg-zinc-50 font-sans dark:bg-slate-900 h-full">
-      <Header/>
-      <WalletConnect/>
+    <div className="h-full flex items-center justify">
+      <div className="w-1/2 px-3 bg-black">
+        <div className="w-full">
+          {!wallet && (
+            <div className="w-full mx-auto rounded-sm justify-between flex mt-4">
+              <h4>Lazit</h4>
+              <p>
+                made with ♥️ by{" "}
+                <Link href={Xlink} className="" target="_blank">
+                  Mofe_bnks
+                </Link>
+              </p>
+            </div>
+          )}
+        </div>
+
+        <WalletConnect />
+      </div>
+      <div className="w-1/2 bg-white text-black justify-between">
+        <div className="min-h-screen text-black flex flex-col justify-center px-3">
+          <h1 className="mb-3 text-3xl">Lazit</h1>
+          <h3>
+            Totally , rad wallet that doesnt require you (yes you 🫵) to keep a
+            seed phrase in some paper
+          </h3>
+          <h4>You dont need a seed phrase before you can make transactions</h4>
+          <h4>Lazit is proof of that</h4>
+          <div className="grid grid-cols-2 grid-rows-2 feature-container gap-3 mt-6">
+            <div className="flex border border-gray-300 px-1.5 py-4 rounded-sm cursor-pointer">
+              Passkey-enabled Wallet
+            </div>
+            <div className="flex border border-gray-300 px-1.5 py-4 rounded-sm cursor-pointer">
+              Integration Guides
+            </div>
+            <div className="flex border border-gray-300 px-1.5 py-4 rounded-sm cursor-pointer">
+              Wallet Sweeping Management
+            </div>
+            <div className="flex border border-gray-300 px-1.5 py-4 rounded-sm cursor-pointer">
+              Feature
+            </div>
+          </div>
+        </div>
+        <p className="bg-black text-white px-3 text-center h-[2%]">
+          Powered by{" "}
+          <a
+            href="https://lazorkit.com/"
+            className="text-[#7857FF] contrast-4.55"
+            target="_blank"
+          >
+            lazorkit
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
--- components/WalletConnect.tsx
+++ components/WalletConnect.tsx
@@ -1,10 +1,15 @@
-"use client"
-import { useWallet } from '@lazorkit/wallet';
+"use client";
+import { useWallet } from "@lazorkit/wallet";
+import { useRouter } from "next/navigation";
 
 export default function WalletConnect() {
-  const {connect , isConnected , isConnecting , wallet , disconnect} = useWallet()
+  const { connect, isConnected, isConnecting, wallet, disconnect } =
+    useWallet();
+  const router = useRouter();
 
   if (isConnected && wallet){
+    router.push("/dashboard");
+
     return(
       <div className="min-h-screen flex items-center justify-center w-full">
         <button className="px-6 py-2 rounded-lg bg-slate-800 text-slate-100 cursor-pointer shadow-lg hover:bg-slate-700 transition-colors duration-200 font-medium text-lg" onClick={() => disconnect()}>
@@ -13,13 +18,16 @@ export default function WalletConnect() {
      </div>
     )
   }
-
-
-    return(
-        <div className="min-h-screen flex items-center justify-center w-full">
-        <button className="px-6 py-2 rounded-lg bg-slate-800 text-slate-100 cursor-pointer shadow-lg hover:bg-slate-700 transition-colors duration-200 font-medium text-lg" onClick={() => connect()}>
-          Connect Wallet
+  else{
+    return (
+      <div className="min-h-screen flex items-center justify-center w-full">
+        <button
+          className="px-6 py-2 rounded-lg bg-slate-800 text-slate-100 cursor-pointer shadow-lg hover:bg-slate-700 transition-colors duration-200 font-medium text-lg"
+          onClick={() => connect()}
+        >
+        {isConnecting ? "Connecting..." : "Connect Wallet"}
         </button>
       </div>
-    )
+    );
+  }
 }
